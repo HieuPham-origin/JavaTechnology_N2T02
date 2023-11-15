5ac46614de62fb83a514266c90b5b7c5d352743a
Hieu Pham-origin <hieupham112003@gmail.com>
2023-11-15 18:00:55 +0700

    add lab8

---
+++ Lab8/exercise1/src/main/java/com/example/exercise1/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.exercise1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping(value = "/contact", method = {RequestMethod.GET, RequestMethod.POST})
+    public String contact(Model model, @RequestParam(required = false) String name, @RequestParam(required = false) String age) {
+        if (name != null && age != null) {
+            model.addAttribute("name", name);
+            model.addAttribute("age", age);
+            return "contact-success";
+        }
+        return "contact";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+}
